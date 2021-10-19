22da65a4e3875b0e5fbbd30b58812db4158a76e0
msfs-junin <msfs_junin@yahoo.com.ar>
2021-10-19 18:37:22 -0300

    Creo background service para obtener cotizaciones desde API externa.

---
+++ TransferOrderAPI/TransferOrderAPI/Services/QuotationsRetrievalService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace API.Services
+{
+    public class QuotationsRetrievalService : BackgroundService
+    {
+        private readonly ILogger<QuotationsRetrievalService> _logger;
+        private readonly string _quotationApiKey = null;
+
+        public QuotationsRetrievalService(ILogger<QuotationsRetrievalService> logger)
+        {
+            _logger = logger;
+            //TODO Inyectar
+            _quotationApiKey = "1124f8b1c4eee98fee0c86571cfd487c";
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("El servicio esta corriendo.");
+            await DoWork(stoppingToken);
+        }
+
+        private async Task DoWork(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Busco cotizacion.");
+            HttpClient http = new HttpClient();
+            //http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("ACCESS_KEY", "1124f8b1c4eee98fee0c86571cfd487c");
+            string url = "http://api.currencylayer.com/live?access_key=" + _quotationApiKey;
+            var apiResponse = http.GetAsync(url).Result.Content.ReadAsStringAsync().Result;
+            if (apiResponse != "")
+            {
+               
+            }
+        }
+
+        public override async Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation(
+                "El servicio se está deteniendo.");
+
+            await base.StopAsync(stoppingToken);
+        }
+     
+
+    }
+}
--- TransferOrderAPI/TransferOrderAPI/Startup.cs
+++ TransferOrderAPI/TransferOrderAPI/Startup.cs
@@ -1,3 +1,4 @@
+using API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,7 @@ namespace TransferOrderAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddHostedService<QuotationsRetrievalService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
